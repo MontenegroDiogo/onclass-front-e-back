89804e9e00e884ca1aa67c4fb8a78628e7a003e2
MontenegroDiogo <diogomonteve@gmail.com>
2025-11-06 23:56:06 -0300

    atualização qrcode quase ok

---
+++ OnClass-sssmain (6)/OnClass-main/js/discenteAdm.js
@@ -0,0 +1,24 @@
+const btnAddAluno = document.getElementById('btn-add-aluno');
+const modalAluno = document.getElementById('modal-aluno');
+const closeModal = document.getElementById('close-modal-aluno');
+const formAluno = document.getElementById('form-aluno');
+const salvarAluno = document.getElementById('salvar-aluno');
+
+// Abrir modal//
+btnAddAluno.addEventListener('click', () => {
+    modalAluno.style.display = 'flex';
+});
+
+// Fechar modal
+closeModal.addEventListener('click', () => {
+    modalAluno.style.display = 'none';
+});
+
+// Fechar modal clicando fora
+window.addEventListener('click', (e) => {
+if (e.target === modalAluno) {
+    modalAluno.style.display = 'none';
+ }
+
+});
+console.log(btnAddAluno, modalAluno, closeModal);
